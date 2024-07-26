e050c48e9ec5dcc1888ba9bcec73e835195432f3
Marc Corthout <marc.corthout+github@gmail.com>
2024-07-26 11:54:25 +0200

    Simplified the list of batteries

--- AfamBikePartsReact/client/components/batteries/batterylist.tsx
+++ AfamBikePartsReact/client/components/batteries/batterylist.tsx
@@ -8,7 +8,6 @@ interface IBatteryListProps {
 }
 
 interface IBatteryListState {
-    collapsed: boolean;
 }
 
 /**
@@ -16,198 +15,98 @@ interface IBatteryListState {
  */
 @observer
 export class BatteryList extends React.Component<IBatteryListProps, IBatteryListState> {
+
+    static readonly titles = [
+        "Item",
+        "EAN",
+        "Technology",
+        "Voltage",
+        "Capacity",
+        "CCA",
+        "AGM",
+        "Gel",
+        "Acid",
+        "Info",
+        "Image",
+    ];
+
     constructor(props: IBatteryListProps) {
         super(props);
-
-        this.state = {
-            collapsed: (window.innerWidth < 800),
-        };
-
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-
-    public componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener("resize", this.updateWindowDimensions);
-    }
-
-    public componentWillUnmount() {
-        window.removeEventListener("resize", this.updateWindowDimensions);
-    }
-
-    public render() {
-        if (this.state.collapsed) {
-            return this.renderCollapsed();
-        }
-        else {
-            return this.renderDefault();
-        }
-    }
-
-    private updateWindowDimensions() {
-        const isCollapsed = (window.innerWidth < 800);
-
-        if (this.state.collapsed !== isCollapsed) {
-            this.setState({
-                collapsed: isCollapsed,
-            });
-        }
     }
 
     private ImageError(item: HTMLImageElement) {
-        if (item) {
-            const parent = item.parentElement;
-            if (parent) {
-                parent.removeChild(item);
-            }
-        }
+        item?.parentElement?.removeChild(item);
     }
 
-    private renderDefault() {
-        let theParts = null;
-
+    public render() {
         const translate = this.props.store.polyglot;
         const batteries = this.props.store.batteries;
 
-        if (batteries) {
-            theParts = batteries.map((s, i) =>
-                <tr key={i.toString()} >
-                    <td data-label="Item">{s.Part}</td>
-                    <td data-label="EAN">{s.EAN}</td>
-                    <td data-label="Type">{s.BatteryType}</td>
-                    <td data-label="Volt">{s.Volt} V</td>
-                    <td data-label="Capacity">{s.Capacity} Ah</td>
-                    <td data-label="CCA">{s.CCA} A</td>
-                    <td data-label="AGM">{s.AGM}</td>
-                    <td data-label="Gel">{s.Gel}</td>
-                    <td data-label="Acid">{s.Acid}</td>
-                    <td data-label="Info" className="centered">
-                        <a href="#">
-                            <img
-                                className="info-image"
-                                src={CommonImageBase + "info.png"}
-                                onClick={(e) => { this.props.store.ShowBatteryInfo(s); e.preventDefault(); }}
-                            />
-                        </a>
-                    </td>
-                    <td data-label="Image" className="centered">
-                        <a href="#">
-                            <img className="part-image"
-                                src={s.Photo}
-                                onError={(e) => this.ImageError(e.currentTarget)}
-                                onClick={(e) => { this.props.store.ShowBatteryImage(s); e.preventDefault(); }}
-                            />
-                        </a>
-                    </td>
-                </tr>,
-            );
+        if (!batteries) {
+            return null;
         }
 
-        return (
-            <div>
-                <h2>{translate.t("ApplicableBatteries")}</h2>
-                <table className="part-table">
-                    <thead>
-                        <tr>
-                            <th>{translate.t("Item")}</th>
-                            <th>{translate.t("EAN")}</th>
-                            <th>{translate.t("Technology")}</th>
-                            <th>{translate.t("Voltage")}</th>
-                            <th>{translate.t("Capacity")}</th>
-                            <th>{translate.t("CCA")}</th>
-                            <th>{translate.t("AGM")}</th>
-                            <th>{translate.t("Gel")}</th>
-                            <th>{translate.t("Acid")}</th>
-                            <th>Info</th>
-                            <th>{translate.t("Image")}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {theParts}
-                    </tbody>
-                </table>
-            </div>
+        // The (translated) headers for the battery attributes
+        const translatedTitles = BatteryList.titles.map((s) => translate.t(s));
+
+        // Create divs that will form the column headers
+        // These column headers will be visible when the data is displayed with each attribute in a column
+        const columnHeaders = translatedTitles.map((s, i) => <div key={"colheader" + i.toString()} className="item-grid-col-header">{s}</div>);
+
+        // Create divs with the actual battery data
+        const parts = batteries.map((s, i) =>
+            [
+                <div key={"Item" + i.toString()} className="item-grid-data" data-label="Item">{s.Part}</div>,
+                <div key={"EAN" + i.toString()} className="item-grid-data" data-label="EAN">{s.EAN}</div>,
+                <div key={"Type" + i.toString()} className="item-grid-data" data-label="Type">{s.BatteryType}</div>,
+                <div key={"Volt" + i.toString()} className="item-grid-data" data-label="Volt">{s.Volt} V</div>,
+                <div key={"Capacity" + i.toString()} className="item-grid-data" data-label="Capacity">{s.Capacity} Ah</div>,
+                <div key={"CCA" + i.toString()} className="item-grid-data" data-label="CCA">{s.CCA} A</div>,
+                <div key={"AGM" + i.toString()} className="item-grid-data" data-label="AGM">{s.AGM}</div>,
+                <div key={"Gel" + i.toString()} className="item-grid-data" data-label="Gel">{s.Gel}</div>,
+                <div key={"Acid" + i.toString()} className="item-grid-data" data-label="Acid">{s.Acid}</div>,
+                <div key={"Info" + i.toString()} className="item-grid-data centered" data-label="Info">
+                    <a href="#">
+                        <img
+                            className="info-image"
+                            src={CommonImageBase + "info.png"}
+                            onClick={(e) => { this.props.store.ShowBatteryInfo(s); e.preventDefault(); }}
+                        />
+                    </a>
+                </div>,
+                <div key={"Image" + i.toString()} className="item-grid-data centered item-grid-row-spacer" data-label="Image">
+                    <a href="#">
+                        <img className="part-image"
+                            src={s.Photo}
+                            onError={(e) => this.ImageError(e.currentTarget)}
+                            onClick={(e) => { this.props.store.ShowBatteryImage(s); e.preventDefault(); }}
+                        />
+                    </a>
+                </div>,
+            ]
         );
-    }
 
-    private renderCollapsed() {
-        let theTables = null;
+        let partList = [];
 
-        const translate = this.props.store.polyglot;
-        const batteries = this.props.store.batteries;
-
-        if (batteries) {
-            theTables = batteries.map((s, i) =>
-                <table className="part-table collapsed" key={i.toString()}>
-                    <tbody>
-                        <tr>
-                            <td>{translate.t("Item")}</td>
-                            <td>{s.Part}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("EAN")}</td>
-                            <td>{s.EAN}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Technology")}</td>
-                            <td data-label="Type" >{s.BatteryType}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Voltage")}</td>
-                            <td data-label="Volt">{s.Volt} V</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Capacity")}</td>
-                            <td data-label="Capacity">{s.Capacity} Ah</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("CCA")}</td>
-                            <td data-label="CCA">{s.CCA} A</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("AGM")}</td>
-                            <td data-label="AGM">{s.AGM}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Gel")}</td>
-                            <td data-label="Gel">{s.Gel}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Acid")}</td>
-                            <td data-label="Acid">{s.Acid}</td>
-                        </tr>
-                        <tr>
-                            <td>Info</td>
-                            <td data-label="Info" className="centered">
-                                <a href="#">
-                                    <img className="info-image"
-                                        src={CommonImageBase + "info.png"}
-                                        onClick={(e) => { this.props.store.ShowBatteryInfo(s); e.preventDefault(); }}
-                                    />
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Image")}</td>
-                            <td data-label="Image" className="centered">
-                                <a href="#">
-                                    <img className="part-image" 
-                                        src={s.Photo}
-                                        onError={(e) => this.ImageError(e.currentTarget)}
-                                        onClick={(e) => { this.props.store.ShowBatteryImage(s); e.preventDefault(); }}
-                                    />
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>,
-            );
-        }
+        // Prepend each data div with a "row header"
+        // These row header divs will be visible when the data is displayed with each attribute in a row
+        for (let i = 0; i < parts.length; i++) {
+            for (let j = 0; j < parts[i].length; j++) {                
+                const rowclass = "item-grid-row-header" + (j === translatedTitles.length - 1 ? " item-grid-row-spacer" : "");
+                const rowkey = `rowheader_${j}_${i}`;
+                const rowheader = <div key={rowkey} className={rowclass}>{translatedTitles[j]}</div>;
+                partList.push(rowheader);
+                partList.push(parts[i][j]);
+            }
+        }       
 
         return (
             <div>
                 <h2>{translate.t("ApplicableBatteries")}</h2>
-                {theTables}
+                <div className="item-grid battery-grid">
+                    {columnHeaders}
+                    {partList}
+                </div>
             </div>
         );
     }
