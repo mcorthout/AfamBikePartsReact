9e41ec5500881a8a6a11e61dc478dd971960ead0
Marc Corthout <marc.corthout+github@gmail.com>
2024-07-29 09:53:02 +0200

    Simplified filter list

--- AfamBikePartsReact/client/components/filters/filterlist.tsx
+++ AfamBikePartsReact/client/components/filters/filterlist.tsx
@@ -8,191 +8,99 @@ interface IFilterListProps {
 }
 
 interface IFilterListState {
-    collapsed: boolean;
 }
 
-/**
- * Container to display a list of batteries
- */
 @observer
 export class FilterList extends React.Component<IFilterListProps, IFilterListState> {
+
+    static readonly titles = [
+        "Item",
+        "EAN",
+        "Type",
+        "Applications",
+        "Image",
+        "Drawing",
+    ];
+
     constructor(props: IFilterListProps) {
         super(props);
-
-        this.state = {
-            collapsed: (window.innerWidth < 800),
-        };
-
-        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-    }
-
-    public componentDidMount() {
-        this.updateWindowDimensions();
-        window.addEventListener("resize", this.updateWindowDimensions);
-    }
-
-    public componentWillUnmount() {
-        window.removeEventListener("resize", this.updateWindowDimensions);
-    }
-
-    public render() {
-        if (this.state.collapsed) {
-            return this.renderCollapsed();
-        }
-        else {
-            return this.renderDefault();
-        }
-    }
-
-    private updateWindowDimensions() {
-        const isCollapsed = (window.innerWidth < 800);
-
-        if (this.state.collapsed !== isCollapsed) {
-            this.setState({
-                collapsed: isCollapsed,
-            });
-        }
     }
 
     private ImageError(item: HTMLImageElement) {
-        if (item) {
-            const parent = item.parentElement;
-            if (parent) {
-                parent.removeChild(item);
-            }
-        }
+        item?.parentElement?.removeChild(item);
     }
 
-    private renderDefault() {
-        let theParts = null;
-
+    public render() {
         const translate = this.props.store.polyglot;
         const filters = this.props.store.filters;
 
-        if (filters) {
-            theParts = filters.map((s, i) =>
-                <tr key={i.toString()} >
-                    <td data-label="Item">{s.Part}</td>
-                    <td data-label="EAN">{s.EAN}</td>
-                    <td data-label="Type">{s.Type}</td>
-                    <td data-label="Applications" className="centered">
-                        <a href="#">
-                            <img
-                                src={CommonImageBase + "bike.png"}
-                                onClick={(e) => { this.props.store.ShowReversedBikes(s); e.preventDefault(); }}
-                            />
-                        </a>
-                    </td>
-                    <td data-label="Image" className="centered">
-                        <a href="#">
-                            <img className="part-image"
-                                src={s.Photo}
-                                onError={(e) => this.ImageError(e.currentTarget)}
-                                onClick={(e) => { this.props.store.ShowFilterImage(s, s.Photo); e.preventDefault(); }}
-                            />
-                        </a>
-                    </td>
-                    <td data-label="Image" className="centered">
-                        <a href="#">
-                            <img className="part-image"
-                                src={s.Drawing}
-                                onError={(e) => this.ImageError(e.currentTarget)}
-                                onClick={(e) => { this.props.store.ShowFilterDrawing(s, s.Drawing); e.preventDefault(); }}
-                            />
-                        </a>
-                    </td>
-                </tr>,
-            );
+        if (!filters) {
+            return null;
         }
 
-        return (
-            <div>
-                <h2>{translate.t("ApplicableFilters")}</h2>
-                <table className="part-table">
-                    <thead>
-                        <tr>
-                            <th>{translate.t("Item")}</th>
-                            <th>{translate.t("EAN")}</th>
-                            <th>{translate.t("Type")}</th>
-                            <th>{translate.t("Applications")}</th>
-                            <th>{translate.t("Image")}</th>
-                            <th>{translate.t("Drawing")}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {theParts}
-                    </tbody>
-                </table>
-            </div>
+        // The (translated) headers for the filter attributes
+        const translatedTitles = FilterList.titles.map((s) => translate.t(s));
+
+        // Create divs that will form the column headers
+        // These column headers will be visible when the data is displayed with each attribute in a column
+        const columnHeaders = translatedTitles.map((s, i) => <div key={"colheader" + i.toString()} className="item-grid-col-header">{s}</div>);
+
+        // Create divs with the actual filter data
+        const parts = filters.map((s, i) =>
+            [
+                <div key={"Item" + i.toString()} className="item-grid-data" data-label="Item">{s.Part}</div>,
+                <div key={"EAN" + i.toString()} className="item-grid-data" data-label="EAN">{s.EAN}</div>,
+                <div key={"Type" + i.toString()} className="item-grid-data" data-label="Type">{s.Type}</div>,
+                <div key={"Applications" + i.toString()} className="item-grid-data centered" data-label="Applications">
+                    <a href="#">
+                        <img
+                            src={CommonImageBase + "bike.png"}
+                            onClick={(e) => { this.props.store.ShowReversedBikes(s); e.preventDefault(); }}
+                        />
+                    </a>
+                </div>,
+                <div key={"Photo" + i.toString()} className="item-grid-data centered" data-label="Image">
+                    <a href="#">
+                        <img className="part-image"
+                            src={s.Photo}
+                            onError={(e) => this.ImageError(e.currentTarget)}
+                            onClick={(e) => { this.props.store.ShowFilterImage(s, s.Photo); e.preventDefault(); }}
+                        />
+                    </a>
+                </div>,
+                <div key={"Drawing" + i.toString()} className="item-grid-data centered item-grid-row-spacer" data-label="Image">
+                    <a href="#">
+                        <img className="part-image"
+                            src={s.Drawing}
+                            onError={(e) => this.ImageError(e.currentTarget)}
+                            onClick={(e) => { this.props.store.ShowFilterDrawing(s, s.Drawing); e.preventDefault(); }}
+                        />
+                    </a>
+                </div>,
+            ]
         );
-    }
 
-    private renderCollapsed() {
-        let theTables = null;
+        let partList = [];
 
-        const translate = this.props.store.polyglot;
-        const filters = this.props.store.filters;
-
-        if (filters) {
-            theTables = filters.map((s, i) =>
-                <table className="part-table collapsed" key={i.toString()}>
-                    <tbody>
-                        <tr>
-                            <td>{translate.t("Item")}</td>
-                            <td>{s.Part}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("EAN")}</td>
-                            <td>{s.EAN}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Type")}</td>
-                            <td data-label="Type" >{s.Type}</td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Applications")}</td>
-                            <td data-label="Applications" className="centered">
-                                <a href="#">
-                                    <img
-                                        src={CommonImageBase + "bike.png"}
-                                        onClick={(e) => { this.props.store.ShowReversedBikes(s); e.preventDefault(); }}
-                                    />
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Image")}</td>
-                            <td data-label="Image" className="centered">
-                                <a href="#">
-                                    <img className="part-image"
-                                        src={s.Photo}
-                                        onError={(e) => this.ImageError(e.currentTarget)}
-                                        onClick={(e) => { this.props.store.ShowFilterImage(s, s.Photo); e.preventDefault(); }}
-                                    />
-                                </a>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>{translate.t("Drawing")}</td>
-                            <td data-label="Drawing" className="centered">
-                                <a href="#">
-                                    <img className="part-image"
-                                        src={s.Drawing}
-                                        onError={(e) => this.ImageError(e.currentTarget)}
-                                        onClick={(e) => { this.props.store.ShowFilterDrawing(s, s.Drawing); e.preventDefault() }}
-                                    />
-                                </a>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>,
-            );
-        }
+        // Prepend each data div with a "row header"
+        // These row header divs will be visible when the data is displayed with each attribute in a row
+        for (let i = 0; i < parts.length; i++) {
+            for (let j = 0; j < parts[i].length; j++) {
+                const rowclass = "item-grid-row-header" + (j === translatedTitles.length - 1 ? " item-grid-row-spacer" : "");
+                const rowkey = `rowheader_${j}_${i}`;
+                const rowheader = <div key={rowkey} className={rowclass}>{translatedTitles[j]}</div>;
+                partList.push(rowheader);
+                partList.push(parts[i][j]);
+            }
+        }      
 
         return (
             <div>
                 <h2>{translate.t("ApplicableFilters")}</h2>
-                {theTables}
+                <div className="item-grid filter-grid">
+                    {columnHeaders}
+                    {partList}
+                </div>
             </div>
         );
     }
